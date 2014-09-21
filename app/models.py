59e09066db6048928bed81e43d50fa67526738d0
cinjon <cinjon.resnick@gmail.com>
2014-09-21 09:39:50 -0700

    models: cron start

--- app/models.py
+++ app/models.py
@@ -3,18 +3,24 @@ from app import utility
 from app import config
 import random
 import os
+import datetime
+from sqlalchemy.sql import func
 from twilio.rest import TwilioRestClient
 import message_options
+from crontab import CronTab
+from flask.ext.script import Command
 
 class Phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_string = db.Column(db.String(25)) #International.
     creation_time = db.Column(db.DateTime)
     messages = db.relationship('Message', lazy='dynamic', backref='phone')
+    deleted = db.Column(db.Boolean)
 
     def __init__(self, phone_string):
         self.phone_string = phone_string
         self.creation_time = utility.get_time()
+        self.deleted = False
 
 def create_phone(phone_string, commit=True):
     phone = Phone(phone_string)
@@ -34,18 +40,16 @@ class Message(db.Model):
         self.selection = selection
         self.creation_time = utility.get_time()
 
-    def send(self):
-        phone = Phone.query.get(self.phone_id)
+    def send(self, phone=None):
+        phone = phone or Phone.query.get(self.phone_id)
         body = message_options.options[self.selection]['body']
-        send_success = send_by_twilio(phone, body)
-        if send_success:
+        if send_by_twilio(phone, body):
             self.sent_time = utility.get_time()
             db.session.commit()
 
-
 def create_message(phone_id, selection=None, commit=True):
     phone = Phone.query.get(phone_id)
-    if not phone:
+    if not phone or phone.deleted:
         return
     selection = selection or get_selection(phone)
     message = Message(selection)
@@ -87,3 +91,33 @@ def send_by_twilio(to_phone, message):
         return True
     except:
         return False
+
+# TODO: Incomplete
+class Cron(Command):
+    """Runs cron for sending messages"""
+    def __init__(self, days_since_msg=7, avg_msg_days=14, max_msg_days=21):
+        self.days_since_msg = days_since_msg # num days since last message
+        self.avg_msg_days = avg_msg_days # avg num of days since last message for any user
+        self.max_msg_days = max_msg_days # max num of days since last message for any user
+
+    def run(self):
+        cron = CronTab()
+
+        try:
+            messages = []
+            for phone in self.get_phones():
+                msg = create_message(phone.id, commit=False)
+                app.db.session.add(message)
+                messages.append((phone, msg))
+            app.db.session.commit()
+
+            for phone, message in messages:
+                message.send(phone)
+        except Exception, e:
+            app.flask_app.logger.debug('Cron failed: %s.' % e)
+            return
+
+    def get_phones(self):
+        today = datetime.datetime.now()
+        latest_msg_by_phone = Message.query(Message.phone_id, func.max(Message.sent_time)).group_by(Message.phone_id)
+        return []
