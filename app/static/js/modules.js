11ee7bc16f9ecd6a809f8452167a4c0d1f1c9ead
cinjon <cinjon.resnick@gmail.com>
2014-09-20 19:30:19 -0700

    Templating changes to reflect fix in ng-app

--- app/static/js/modules.js
+++ app/static/js/modules.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('deathmsg', ['deathmsgServices', 'deathmsgFilters', 'deathmsgDirectives', 'ui.bootstrap', 'angular-loading-bar'])
+angular.module('DeathMsg', ['deathmsgServices', 'deathmsgFilters', 'deathmsgDirectives', 'ui.bootstrap', 'angular-loading-bar'])
   .controller('main', function($scope) {
 
   })
