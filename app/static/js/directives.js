a8d6ee0a2367dded28c19f4e51ad715c007f9f49
cinjon <cinjon.resnick@gmail.com>
2014-09-20 20:33:34 -0700

    phone formatting

--- app/static/js/directives.js
+++ app/static/js/directives.js
@@ -16,4 +16,32 @@ angular.module('deathmsgDirectives', [])
         });
       }
     };
-  });
+  })
+  .directive('phoneformat', ['$filter', function($filter) {
+    return {
+      require: '?ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        if (!ctrl) return;
+        ctrl.$parsers.unshift(function (viewValue) {
+          var plainNumber = viewValue.replace(/[^\d+]/g, '');
+          var st = plainNumber.toString();
+          if (st.length >= 10) {
+            st = st.substring(0,10);
+          }
+          elem.val(getFormattedPhone(st));
+          return parseInt(st);
+        });
+      }
+    };
+  }]);
+
+var getFormattedPhone = function(str) {
+  var len = str.length;
+  if (len < 3) {
+    return str;
+  } else if (len >= 3 && len <= 6) {
+    return '(' + str.slice(0,3) + ') - ' + str.slice(3);
+  } else {
+    return '(' + str.slice(0,3) + ') - ' + str.slice(3,6) + ' - ' + str.slice(6,10)
+  }
+}
