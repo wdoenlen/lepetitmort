170db186c23d1e545b8e288762c9663a6034d272
cinjon <cinjon.resnick@gmail.com>
2014-09-27 07:27:40 -0700

    cron continuing tests

--- app/scripts/cron.py
+++ app/scripts/cron.py
@@ -4,18 +4,20 @@ import random
 import datetime
 from sqlalchemy.sql import func
 
-min_msg_days = 7 # num days since last message
-max_msg_days = 14 # max num of days since last message for any user
+min_msg_days = 7 # min num days since last message
+max_msg_days = 14 # max num days since last message
 
-def test():
-    app.flask_app.logger.debug('Hi in test')
+def greet():
+    app.flask_app.logger.debug('Cron: Hi')
 
 def run(min_days=None, max_days=None, test=False):
     min_days = min_days or min_msg_days
     max_days = max_days or max_msg_days
 
     messages = []
-    for phone_id in get_phones_on_tap(min_days, max_days):
+    phones = get_phones_on_tap(min_days, max_days)
+    app.flask_app.logger.debug('Cron: Creating %d Messages' % len(phones))
+    for phone_id in phones:
         try:
             msg = app.models.create_message(phone_id, commit=False)
             app.db.session.add(msg)
@@ -24,22 +26,24 @@ def run(min_days=None, max_days=None, test=False):
         except Exception, e:
             app.flask_app.logger.debug('Failed to load message for phone %d' % phone_id)
             app.flask_app.logger.debug(e)
+    app.flask_app.logger.debug('Cron: Finished Creating Messages')
 
     if not test:
+        app.flask_app.logger.debug('Cron: Committing')
         app.db.session.commit()
 
+    app.flask_app.logger.debug('Cron: Sending Messages')
     for phone_id, message in messages:
         try:
             if test:
                 app.flask_app.logger.debug(message.get_body())
             else:
                 message.send(app.models.Phone.query.get(phone_id))
+                app.db.session.commit()
         except Exception, e:
             app.flask_app.logger.debug('Failed to send message with selection %d for phone %d' % (message.selection, phone_id))
             app.flask_app.logger.debug(e)
-
-    if not test:
-        app.db.session.commit()
+    app.flask_app.logger.debug('Cron: Sent Messages')
 
 def get_phones_on_tap(min_days, max_days):
     ret = []
@@ -48,7 +52,7 @@ def get_phones_on_tap(min_days, max_days):
     delta_max = datetime.timedelta(max_days)
     for phone_id, time in app.db.session.query(Message.phone_id, func.max(Message.sent_time)).group_by(Message.phone_id):
         if not phone_id or not time:
-            app.flask_app.logger.warn('WARN get_phones_on_tap had no value on phone_id ' + phone_id + ' or time ' + time)
+            # Likely a bad phone number which then didn't send an intro message
             continue
         delta = today - time
         if delta < delta_min:
